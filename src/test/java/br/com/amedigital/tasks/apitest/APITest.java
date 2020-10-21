4578e958df072be1cbd6e6b1ac86a8acb37c05d3
Adriano Vieira <adriano.savoir@gmail.com>
2020-10-21 00:04:57 -0300

    first commit

---
+++ src/test/java/br/com/amedigital/tasks/apitest/APITest.java
@@ -0,0 +1,56 @@
+package br.com.amedigital.tasks.apitest;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import sun.util.logging.resources.logging;
+
+public class APITest {
+	
+	@BeforeClass
+	public static void setup() {
+		RestAssured.baseURI = "http://localhost:8001/tasks-backend";
+	}
+	
+	@Test
+	public void deveRetornarTarefas() {
+		
+		RestAssured.given()
+		.when()
+			.get("/todo")
+		.then()
+			.statusCode(200)
+		;		
+	}
+	
+	@Test
+	public void deveAdicionarTarefaComSucesso() {
+		
+		RestAssured.given()
+			.body("{ \"task\": \"teste via api\", \"dueDate\": \"2020-12-20\" }")
+			.contentType(ContentType.JSON)
+		.when()
+			.post("/todo")
+		.then()
+			.statusCode(201)
+		;		
+	}
+	
+	@Test
+	public void naoDeveAdicionarTarefaInvalida() {
+		
+		RestAssured.given()
+			.body("{ \"task\": \"teste via api\", \"dueDate\": \"2010-12-20\" }")
+			.contentType(ContentType.JSON)
+		.when()
+			.post("/todo")
+		.then()
+			.statusCode(400)
+			.body("message", CoreMatchers.is("Due date must not be in past"))
+		;		
+	}
+
+}
